c943f17aa4abb2ed498bd917cf14ee5c1b8f95aa
teotix <teo_kostic@yahoo.com>
2023-09-15 17:53:46 +0200

    Add game logic

--- game.js
+++ game.js
@@ -8,25 +8,33 @@ function getComputerChoice() {
   return choice;
 }
 
+function capitalize(string) {
+  let lower = string.toLowerCase();
+  return lower[0].toUpperCase() + lower.slice(1);
+}
+
 function getUserChoice() {
   let choicePrompt = prompt("Rock, Paper or Scissors?");
   choicePrompt == "" || choicePrompt == null ? getUserChoice() : "";
-  return choicePrompt;
+  return capitalize(choicePrompt);
 }
 
-// function playGame(playerSelection, computerSelection) {
-//   if (
-//     (playerSelection == "Rock" && computerSelection == "Scissors") ||
-//     (playerSelection == "Paper" && computerSelection == "Rock") ||
-//     (playerSelection == "Scissors" && computerSelection == "Paper")
-//   ) {
-//     return `Congratulations`
-//   }
-// }
+function playGame(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "Rock" && computerSelection == "Scissors") ||
+    (playerSelection == "Paper" && computerSelection == "Rock") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return `Draw! You both chose ${playerSelection}`;
+  } else if (!["Rock", "Paper", "Scissors"].includes(playerSelection)) {
+    return "You have chosen an invalid value!";
+  } else return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
 
 let userChoice = getUserChoice();
 
 let computerChoice = getComputerChoice();
 
-console.log(userChoice);
-console.log(computerChoice);
+console.log(playGame(userChoice, computerChoice));
